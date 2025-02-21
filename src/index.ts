8bd952851eb2c2b5b56b964d2a2f349492239dc8
jeessicats <jeessicats@gmail.com>
2025-02-21 14:11:36 -0300

    CustomMap structure

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
+/// <reference types="google.maps" />
+
 import { User } from './User';
-import { Company  } from './Company';
+import { Company } from './Company';
+import { CustomMap } from './CustomMap';
 
 const user = new User();
-
 console.log(user);
 
 const company = new Company();
-
 console.log(company);
 
-google
+new CustomMap('map');
